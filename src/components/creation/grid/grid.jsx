75676fd51aca84c7034ba8967c69e905a2119f51
renzorr <benaventer01@gmail.com>
2023-04-14 20:33:07 -0500

    fix image

--- src/components/creation/grid/grid.jsx
+++ src/components/creation/grid/grid.jsx
@@ -1,53 +1,70 @@
 import Media from "react-media";
 import "./grid.scss";
-import IMagen from "../../../../images/desktop/image-deep-earth.jpg";
+
+import Deep from "../../../../images/desktop/image-deep-earth.jpg";
+import DeepMobile from "../../../../images/mobile/image-deep-earth.jpg";
+import Night from "../../../../images/desktop/image-night-arcade.jpg";
+import NightMobile from "../../../../images/mobile/image-night-arcade.jpg";
+import Soccer from "../../../../images/desktop/image-soccer-team.jpg";
+import SoccerMobile from "../../../../images/mobile/image-soccer-team.jpg";
+import GridImg from "../../../../images/desktop/image-grid.jpg";
+import GridMobile from "../../../../images/mobile/image-grid.jpg";
+import AboveImg from "../../../../images/desktop/image-from-above.jpg";
+import AboveMobile from "../../../../images/mobile/image-grid.jpg";
+import Pocket from "../../../../images/desktop/image-pocket-borealis.jpg";
+import PocketMobile from "../../../../images/mobile/image-pocket-borealis.jpg";
+import Curiosity from "../../../../images/desktop/image-curiosity.jpg";
+import CuriosityMobile from "../../../../images/mobile/image-curiosity.jpg";
+import MakeImg from "../../../../images/desktop/image-fisheye.jpg";
+import MakeMobile from "../../../../images/mobile/image-fisheye.jpg";
+
 const galleryObj = [
   {
     id: 1,
-    ImageAddress: "../../../../images/desktop/image-deep-earth.jpg",
-    ImageAddressMobile: "../../../../images/mobile/image-deep-earth.jpg",
+    ImageAddress: Deep,
+    ImageAddressMobile: DeepMobile,
     ImageName: "DEEP EARTH",
   },
   {
     id: 2,
-    ImageAddress: "../../../../images/desktop/image-night-arcade.jpg",
-    ImageAddressMobile: "../../../../images/mobile/image-night-arcade.jpg",
+    ImageAddress: Night,
+    ImageAddressMobile: NightMobile,
     ImageName: "NIGHT ARCADE",
   },
   {
     id: 3,
-    ImageAddress: "../../../../images/desktop/image-soccer-team.jpg",
-    ImageAddressMobile: "../../../../images/mobile/image-soccer-team.jpg",
+    ImageAddress: Soccer,
+    ImageAddressMobile: SoccerMobile,
     ImageName: "SOCCER TEAM VR",
   },
   {
     id: 4,
-    ImageAddress: "../../../../images/desktop/image-grid.jpg",
-    ImageAddressMobile: "../../../../images/mobile/image-grid.jpg",
+    ImageAddress: GridImg,
+    ImageAddressMobile: GridMobile,
     ImageName: " THE GRID",
   },
   {
     id: 5,
-    ImageAddress: "../../../../images/desktop/image-from-above.jpg",
-    ImageAddressMobile: "../../../../images/mobile/image-from-above.jpg",
+    ImageAddress: AboveImg,
+    ImageAddressMobile: AboveMobile,
     ImageName: "FROM UP ABOVE VR ",
   },
   {
     id: 6,
-    ImageAddress: "../../../../images/desktop/image-pocket-borealis.jpg",
-    ImageAddressMobile: "../../../../images/mobile/image-pocket-borealis.jpg",
+    ImageAddress: Pocket,
+    ImageAddressMobile: PocketMobile,
     ImageName: "POCKET BOREALIS",
   },
   {
     id: 7,
-    ImageAddress: "../../../../images/desktop/image-curiosity.jpg",
-    ImageAddressMobile: "../../../../images/mobile/image-curiosity.jpg",
+    ImageAddress: Curiosity,
+    ImageAddressMobile: CuriosityMobile,
     ImageName: "THE CURIOSITY ",
   },
   {
     id: 8,
-    ImageAddress: "../../../../images/desktop/image-fisheye.jpg",
-    ImageAddressMobile: "../../../../images/mobile/image-fisheye.jpg",
+    ImageAddress: MakeImg,
+    ImageAddressMobile: MakeMobile,
     ImageName: "MAKE IT FISHEYE ",
   },
 ];
@@ -58,7 +75,7 @@ function Desktop() {
       {galleryObj.map((e) => {
         return (
           <div className="cart" key={e.id}>
-            <img src={IMagen} alt={`${e.ImageName}`} />
+            <img src={e.ImageAddress} alt={`${e.ImageName}`} />
             <p className="title">{e.ImageName}</p>
           </div>
         );
